8e64d455cecf8ec0e33a6663d7fe5cd743f79a6a
agustin3a94 <agustin3a94@gmail.com>
2014-09-21 13:20:05 -0600

    Index modificado y agregada navbar

--- index.php
+++ index.php
@@ -1,9 +1,39 @@
-<?php include 'include/_header.html' ?>
+<?php 
+	include 'include/_header.html';
+	include 'layouts/navbar.html';
+?>
 
-<div class="container">
-	<div class="jumbotron">
-		<a href="ne.php">ne</a>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+			<h3 class="ch3">Vuelos</h3>
+				<a href="vuelos.php" class="thumbnail"><img src="icons/vuelos.svg"></a>
+      </div>
+  		<div class="col-md-4">
+  			<h3 class="ch3">Registro</h3>
+				<a href="vuelos.php" class="thumbnail"><img src="icons/registro.svg"></a>
+  		</div>
+  		<div class="col-md-4">
+  			<h3 class="ch3">Pantalla</h3>
+  			<a href="vuelos.php" class="thumbnail"><img src="icons/screen.svg"></a>
+  		</div>
+  	</div>
+  	<div class="row">
+			<div class="col-md-4">
+				<h3 class="ch3">Configuracion</h3>
+				<a href="vuelos.php" class="thumbnail"><img src="icons/vuelos.svg"></a>
+  		</div>
+  		<div class="col-md-4">
+  			<h3 class="ch3">Configuracion</h3>
+				<a href="vuelos.php" class="thumbnail"><img src="icons/settings.svg"></a>
+  		</div>
+  		<div class="col-md-4">
+  			<h3 class="ch3">Salir</h3>
+  			<a href="vuelos.php" class="thumbnail"><img src="icons/salir.svg"></a>
+  		</div>
+  	</div>
 	</div>
-</div>
+
 
 <?php include 'include/_footer.html' ?>
